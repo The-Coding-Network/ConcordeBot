1bf96fdaa0f0e93c6f5a8883289cddeda47b63de
jameshaswellukcom <james@haswelluk.com>
2021-10-14 15:58:30 +0100

    command handler

--- backend/src/handler/command.js
+++ backend/src/handler/command.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = (client, Discord) => {
 
     const load_dir = (dirs) =>{
-        const command_files = fs.readdirSync(`./commands/${dirs}`).filter(file => file.endsWith('.js'));
+        const command_files = fs.readdirSync(`./src/commands/${dirs}`).filter(file => file.endsWith('.js'));
         
         for(const file of command_files){
             const command = require(`../commands/${dirs}/${file}`);
@@ -11,7 +11,7 @@ module.exports = (client, Discord) => {
         }
         
     }
-    [].forEach(e => load_dir(e));
+    ['owner', 'config'].forEach(e => load_dir(e));
     //Add your directory names to add catagories to your commands this makes your project cleaner
 
 }
